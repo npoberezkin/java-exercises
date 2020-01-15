35fb1aeef5486a50d003b3001271739a45f9e600
Nikita_Poberezkin <Nikita_Poberezkin@epam.com>
2020-01-15 14:32:26 +0300

    master: fixed build

--- src/main/java/streamapi/domain/Country.java
+++ src/main/java/streamapi/domain/Country.java
@@ -1,8 +1,15 @@
 package streamapi.domain;
 
+import lombok.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Country {
     private String code;
     private String name;
@@ -12,92 +19,4 @@ public class Country {
     private double gnp;
     private int capital;
     private List<City> cities;
-
-    {
-        cities = new ArrayList<City>();
-    }
-
-    public Country() {
-    }
-
-    public Country(String code, String name, String continent, int population,
-                   double surfaceArea, double gnp, int capital) {
-        this.code = code;
-        this.name = name;
-        this.continent = continent;
-        this.surfaceArea = surfaceArea;
-        this.population = population;
-        this.capital = capital;
-        this.gnp = gnp;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContinent() {
-        return continent;
-    }
-
-    public void setContinent(String continent) {
-        this.continent = continent;
-    }
-
-    public double getSurfaceArea() {
-        return surfaceArea;
-    }
-
-    public void setSurfaceArea(double surfaceArea) {
-        this.surfaceArea = surfaceArea;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
-    public double getGnp() {
-        return gnp;
-    }
-
-    public void setGnp(double gnp) {
-        this.gnp = gnp;
-    }
-
-    public int getCapital() {
-        return capital;
-    }
-
-    public void setCapital(int capital) {
-        this.capital = capital;
-    }
-
-    public List<City> getCities() {
-        return cities;
-    }
-
-    public void setCities(List<City> cities) {
-        this.cities = cities;
-    }
-
-    @Override
-    public String toString() {
-        return "Country [ name=" + name + ", population=" + population + "]";
-    }
-
 }
