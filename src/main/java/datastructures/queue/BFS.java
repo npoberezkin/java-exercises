d293b57ba9c40bb809f55bde0b81dc8bfa91b9f9
Nikita_Poberezkin <Nikita_Poberezkin@epam.com>
2020-05-06 10:56:28 +0300

    master: datastructures -> queue : BFS - visit before addition instead of after removal

--- src/main/java/datastructures/queue/BFS.java
+++ src/main/java/datastructures/queue/BFS.java
@@ -10,17 +10,22 @@ public class BFS {
         Tree<String> tree = new Tree<>("root");
         Node<String> node_1 = tree.root().add("node_1");
         Node<String> node_2 = tree.root().add("node_2");
-        node_1.add("node_1_1");
+        Node<String> node_1_1 = node_1.add("node_1_1");
         node_1.add("node_1_2");
         node_2.add("node_2_1");
+        node_1_1.add("node_1_1_1");
         System.out.println(tree);
 
         LinkedList<Node<String>> nodes = new LinkedList<>();
         nodes.add(tree.root());
+        tree.root().visit();
+        System.out.println(tree);
         while (!nodes.isEmpty()) {
             Node<String> frontier = nodes.remove();
-            frontier.visit();
-            nodes.addAll(frontier.getChildren());
+            for (Node<String> node : frontier.getChildren()) {
+                node.visit();
+                nodes.add(node);
+            }
             System.out.println(tree);
         }
     }
